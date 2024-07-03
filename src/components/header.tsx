a2590034f887fec8a5c66312610a0bb263dc4459
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-07-02 23:50:29 -0500

    update header with logo and bg blur

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,25 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Text } from "@chakra-ui/react";
 
 export default function Header() {
   return (
-    <Box>
-      <Image
-        src={"./image.png"}
-        alt="avatar aang with two blades"
-        boxSize={"35px"}
-      />
-    </Box>
+    <header>
+      <Box
+        w={"100%"}
+        borderBottomWidth={1}
+        py={4}
+        backdropFilter="blur(10px) hue-rotate(90deg)"
+      >
+        <Box display={"flex"} alignItems={"center"} pl={4}>
+          <Image
+            src={"./image.png"}
+            alt="avatar aang with two blades"
+            boxSize={"35px"}
+          />
+          <Text as={"h1"} fontSize={"xl"} fontWeight={"bold"}>
+            n0ne
+          </Text>
+        </Box>
+      </Box>
+    </header>
   );
 }
