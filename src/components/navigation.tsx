d011a0130cd32861b25c1f30b61dffedb8304ccd
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-07-06 00:17:52 -0500

    add accoridon

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,23 +1,50 @@
-import { Box, Link, Text } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function Navigation() {
   return (
     <nav>
-      <Box padding={5} height={"100%"} mt={20} pos={"fixed"}>
+      <Box padding={4} height={"100%"} mt={20} pos={"fixed"}>
         <Link href="/">
-          <Text as={"h2"} fontSize={"2xl"}>
+          <Text as={"h2"} fontSize={"2xl"} p={1}>
             whoami
           </Text>
         </Link>
 
         <Box mt={2}>
-          <Link>WriteUps CTF</Link>
-          <Box paddingLeft={4} display={"flex"} flexDir={"column"}>
-            <Link href="/#">something</Link>
-            <Link href="/#">something</Link>
-            <Link href="/#">something</Link>
-          </Box>
+          <Accordion allowMultiple borderWidth={0} padding={0} m={0}>
+            <AccordionItem borderWidth={0} padding={0} m={0}>
+              <h2>
+                <AccordionButton padding={1}>
+                  <Box
+                    as="span"
+                    padding={0}
+                    m={0}
+                    flex={1}
+                    textAlign={"left"}
+                    borderWidth={0}
+                  >
+                    WriteUps CTF
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel display={"flex"} flexDir={"column"}>
+                <Link href="/#">something</Link>
+                <Link href="/#">something</Link>
+                <Link href="/#">something</Link>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
         </Box>
       </Box>
     </nav>
