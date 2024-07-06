0b097bc5b43d6461f0e35f191d05850777e3639b
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-07-06 00:15:43 -0500

    add menu button to handle links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,62 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+"use client";
+
+import { HamburgerIcon, SmallCloseIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  IconButton,
+  Image,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Show,
+  Text,
+} from "@chakra-ui/react";
 
 export default function Header() {
   return (
     <header>
       <Box
-        w={"100%"}
         borderBottomWidth={1}
         py={4}
         backdropFilter="blur(10px) hue-rotate(90deg)"
       >
-        <Box display={"flex"} alignItems={"center"} pl={4}>
-          <Image
-            src={"./image.png"}
-            alt="avatar aang with two blades"
-            boxSize={"35px"}
-          />
-          <Text as={"h1"} fontSize={"xl"} fontWeight={"bold"}>
-            n0ne
-          </Text>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          pl={4}
+          justifyContent={"space-between"}
+        >
+          <Box display={"flex"}>
+            <Image
+              src={"./image.png"}
+              alt="avatar aang with two blades"
+              boxSize={"35px"}
+            />
+            <Text as={"h1"} fontSize={"xl"} fontWeight={"bold"}>
+              n0ne
+            </Text>
+          </Box>
+          <Show below="md">
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant={"outline"}
+              />
+              <MenuList>
+                <MenuItem>
+                  <Link href="/">whoami</Link>
+                </MenuItem>
+
+                <MenuItem>
+                  <Link href="/#"> WriteUps CTF</Link>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Show>
         </Box>
       </Box>
     </header>
